5323dbc39473f34db837c90907fb7ff97d2f112a
Mateusz Furga <matfurga@gmail.com>
2021-06-22 18:32:57 +0200

    Update quickselect

--- algo/sortowania/quickselect.py
+++ algo/sortowania/quickselect.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python3
-
+#
+# Szukanie i-tego co do wielkości elementu w czasie O(n) w nieposortowanie tablicy.
+#
 from random import randint
 
-def partition(arr, l, r):
+def partition(A, l, r):
   q = randint(l, r)
-  arr[q], arr[r] = arr[r], arr[q]
-
-  p = arr[r]
+  A[q], A[r] = A[r], A[q]
+  p = A[r]
   i = l - 1
-
   for j in range(l, r):
-    if arr[j] < p:
+    if A[j] <= p:
       i += 1
-      arr[i], arr[j] = arr[j], arr[i]
-
-  arr[i + 1], arr[r] = arr[r], arr[i + 1]
+      A[i], A[j] = A[j], A[i]
+  A[i + 1], A[r] = A[r], A[i + 1]
   return i + 1
 
-def _quickselect(arr, l, r, k):
+def _quickselect(A, l, r, k):
   if l == r:
-    return arr[l]
+    return A[l]
+  q = partition(A, l, r)
+  if q == k:
+    return A[q]
+  if q < k:
+    return _quickselect(A, q + 1, r, k)
+  if q > k:
+    return _quickselect(A, l, q - 1, k)
 
-  p = partition(arr, l, r)
-
-  if p == k:
-    return arr[p]
-
-  if p < k:
-    return _quickselect(arr, l + 1, r, k)
-
-  if p > k:
-    return _quickselect(arr, l, r - 1, k)
-
-def quickselect(arr, k):
-  n = len(arr)
-  return _quickselect(arr, 0, n - 1, k)
+def quickselect(A, k):
+  return _quickselect(A, 0, len(A) - 1, k)
 
