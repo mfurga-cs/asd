3af9dea10f536b7e28cd6f87495cdc2804960d4d
Mateusz Furga <matfurga@gmail.com>
2021-06-29 14:19:52 +0200

    Zaba dzialagit add fog.py

--- algo/dynamiczne/fog.py
+++ algo/dynamiczne/fog.py
@@ -3,27 +3,30 @@
 # f(i, y) - min. liczba skoków potrzebna by dotrzeć do liczby i mająć w zapasie dokładnie y
 #           jednostek energii.
 #
-# f(i, y) = min(0 <= j < i) { f(j, y + (i - j) - A[i] } + 1
-# f(0, y) = 0    gdy y <= A[0]
-# f(0, y) = inf  gdy y >  A[0]
+# f(i, y) = min(0 <= j < i) { f(j, max{ y + (i - j) - A[i], 0} } + 1
+# f(0, y) = 0    gdy y <= 0
+# f(0, y) = inf  gdy y >  0
+
+from zad1testy import runtests
 
 def result(P, A, i, y):
-  if P[i][y] is None:
-    return []
-  return result(P, A, P[i][y], y + (i - P[i][y]) - A[i]) + [i]
+  if i == 0:
+    return [0]
+  j = P[i][y]
+  return result(P, A, j, max(y + (i - j) - A[j], 0)) + [i]
 
 def f(F, P, A, i, y):
-  if i == 0 and y <= A[0]:
-    return 0
   if i == 0:
+    if y <= 0:
+      return 0
     return float("+inf")
 
-  if F[i][y] != float("+inf"):
+  if F[i][y] is not None:
     return F[i][y]
 
   F[i][y] = float("+inf")
   for j in range(i):
-    v = f(F, P, A, j, y + (i - j) - A[i])
+    v = f(F, P, A, j, max(y + (i - j) - A[j], 0))
     if v + 1 < F[i][y]:
       F[i][y] = v + 1
       P[i][y] = j
@@ -37,11 +40,8 @@ def fwrapper(A):
 
   maxy = sum(A)
   for i in range(n):
-    F[i] = [float("+inf")] * (maxy + 1)
+    F[i] = [None] * (maxy + 1)
     P[i] = [None] * (maxy + 1)
-
-  for y in range(A[0] + 1):
-    F[0][y] = 0
 
   v = f(F, P, A, len(A) - 1, 0)
   return result(P, A, len(A) - 1, 0)
@@ -49,5 +49,10 @@ def fwrapper(A):
 A = [2,2,1,0,0,0]
 A = [4,2,2,4,1,2,1,0]
 A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+A = [2,2,1,0,0,0]
+A = [4,5,2,4,1,2,1,0]
 print(fwrapper(A))
+
+#runtests(fwrapper)
+
 
