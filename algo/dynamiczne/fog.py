371fb03785bc1235b6dc4b7816ca7f3ed764bcc9
Mateusz Furga <matfurga@gmail.com>
2021-07-08 18:37:47 +0200

    Update fog

--- algo/dynamiczne/fog.py
+++ algo/dynamiczne/fog.py
@@ -1,58 +1,55 @@
 #!/usr/bin/env python3
 #
-# f(i, y) - min. liczba skoków potrzebna by dotrzeć do liczby i mająć w zapasie dokładnie y
-#           jednostek energii.
+# f(i, k) - min. liczba skoków potrzebna aby dotrzeć na pole i-te i mając na nim po
+#           zjedzeniu k jednostek energii.
 #
-# f(i, y) = min(0 <= j < i) { f(j, max{ y + (i - j) - A[i], 0} } + 1
-# f(0, y) = 0    gdy y <= 0
-# f(0, y) = inf  gdy y >  0
+# f(i, k) = min(f(j, k + (i - j) - A[i]) + 1)
+# Musimy zadbać o to aby energie na polu i-tym nie była mniejsza niż wartość na nim.
+# Tzn. k = max(k + (i - j) - A[i], A[j])
+#
+# ODP: f(n - 1, A[n - 1])
 
 from zad1testy import runtests
 
-def result(P, A, i, y):
-  if i == 0:
-    return [0]
-  j = P[i][y]
-  return result(P, A, j, max(y + (i - j) - A[j], 0)) + [i]
+def get_solution(A, P, i, k):
+  if P[i][k] is None:
+    return [i]
+  ii, kk = P[i][k]
+  kk = max(kk, A[ii])
+  return get_solution(A, P, ii, kk) + [i]
 
-def f(F, P, A, i, y):
+def f(F, P, A, i, k):
   if i == 0:
-    if y <= 0:
+    if k <= A[0]:
       return 0
     return float("+inf")
 
-  if F[i][y] is not None:
-    return F[i][y]
+  # Nie możemy mieć mniej energii na polu i-tym niż A[i].
+  if k < A[i]:
+    k = A[i]
 
-  F[i][y] = float("+inf")
+  if F[i][k] is not None:
+    return F[i][k]
+
+  F[i][k] = float("+inf")
   for j in range(i):
-    v = f(F, P, A, j, max(y + (i - j) - A[j], 0))
-    if v + 1 < F[i][y]:
-      F[i][y] = v + 1
-      P[i][y] = j
-
-  return F[i][y]
+    v = f(F, P, A, j, k + (i - j) - A[i]) + 1
+    if v < F[i][k]:
+      F[i][k] = v
+      P[i][k] = (j, k + (i - j) - A[i])
+  return F[i][k]
 
 def fwrapper(A):
   n = len(A)
+  s = sum(A)
   F = [None] * n
   P = [None] * n
-
-  maxy = sum(A)
   for i in range(n):
-    F[i] = [None] * (maxy + 1)
-    P[i] = [None] * (maxy + 1)
+    F[i] = [None] * s
+    P[i] = [None] * s
+  v = f(F, P, A, n - 1, A[n - 1])
+  print(get_solution(A, P, n - 1, A[n - 1]))
+  return v
 
-  v = f(F, P, A, len(A) - 1, 0)
-  return result(P, A, len(A) - 1, 0)
-
-A = [2,2,1,0,0,0]
-A = [4,2,2,4,1,2,1,0]
-A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-A = [2,2,1,0,0,0]
-A = [4,5,2,4,1,2,1,0]
-print(fwrapper(A))
-
-#runtests(fwrapper)
-
+runtests(fwrapper)
 
