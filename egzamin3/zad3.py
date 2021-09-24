a250ea4e5cd3d943504e46a3894935d59ef69c68
Mateusz Furga <matfurga@gmail.com>
2021-09-24 18:20:25 +0200

    EGZAMIN3: Zad3

--- egzamin3/zad3.py
+++ egzamin3/zad3.py
@@ -1,11 +1,26 @@
 from zad3testy import runtests
 
+def minpow2(n):
+  p = 1
+  while p <= n:
+    p *= 2
+  return p // 2
 
-def maxim( T, C ):
-    """tu prosze wpisac wlasna implementacje"""
-    return -1
+def find(T, c, p):
+  if p == 0:
+    return T.key
+  if c & p == 0:
+    return find(T.left, c, p // 2)
+  else:
+    return find(T.right, c, p // 2)
 
-    
-runtests( maxim )
+def maxim(T, C):
+  m = -1
+  for c in C:
+    p = minpow2(c)
+    v = find(T, c, p // 2)
+    m = max(m, v)
+  return m
 
+runtests(maxim)
 
